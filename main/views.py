bd846dc6d89a386391811588074a5719560fa6eb
Your Name <you@example.com>
2024-11-07 17:46:58 +0500

    popa

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def index(request):
     data = {
         'title': 'Главная страница',
-        'values': ['asdafs', 'aefefefef', 'AIHKFVBAKFVBIAVB']
+        'values': ['Пися', 'Попа', 'чилеееен']
     }
     return render(request, "main/index.html", data)
 
